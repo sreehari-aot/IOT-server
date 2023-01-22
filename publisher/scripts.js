4efe7484626baa85d8a3f72d1d9ff3d3d140b509
sreehari ps <sreehari.ps@aot-technologies.com>
2023-01-22 07:16:54 +0530

    Update publisher scripts
    Update scripts so that instead of all at once,
    publish the new data points at 20 second
    intervals. This best mocks an IOT device
    in action.

--- publisher/scripts.js
+++ publisher/scripts.js
@@ -3,11 +3,14 @@ const axios = require('axios');
 const dataUrl = 'http://localhost:8000/data/set1.json';
 const postUrl = 'http://localhost:8000/publish';
 
+let index = 0;
 
 const postData = (url, data) => {
     axios.post(url, data)
       .then((response) =>{
-        console.log("Request posted successfully");
+        if(response.status === 201){
+            console.log("Request posted successfully");
+        }
       })
       .catch(error => {
         console.log("Failed to post request",error);
@@ -27,21 +30,23 @@ const getData = (url, done)=>{
 }
 
 
-getData(dataUrl, async (data)=>{
-    data.forEach(element => {
-        let entry = {...element}
-        for (let instance in entry.instances) {
-            let payload = {
-                "timeStamp":entry.timestamp.$date.$numberLong,
-                "person": instance,
-                "pos_x": entry.instances[instance]["pos_x"],
-                "pos_y": entry.instances[instance]["pos_y"],
-                "vel_x": entry.instances[instance]["vel_x"],
-                "vel_y": entry.instances[instance]["vel_y"],
-                "o_id": entry._id.$oid
+setInterval(()=>{
+    getData(dataUrl, async (data)=>{
+            let element = data[index];
+            let entry = {...element}
+            for (let instance in entry.instances) {
+                let payload = {
+                    "timeStamp":entry.timestamp.$date.$numberLong,
+                    "person": instance,
+                    "pos_x": entry.instances[instance]["pos_x"],
+                    "pos_y": entry.instances[instance]["pos_y"],
+                    "vel_x": entry.instances[instance]["vel_x"],
+                    "vel_y": entry.instances[instance]["vel_y"],
+                    "o_id": entry._id.$oid
+                }
+                postData(postUrl, payload)
             }
-            postData(postUrl, payload)
-        }
-        
-    });
-})
+            
+    })
+    index++
+},20000)
